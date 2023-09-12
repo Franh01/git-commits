458e075e255b544eb143db782f31e4ddf1300030
Franh01 <fran_lopez9@live.com>
2023-09-12 19:43:46 -0300

    Update styles. Styles updated in Commit component and index.css

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import Commit from "./components/Commit/Commit";
-import { Box, Typography } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 
 function App() {
   const [commits, setCommits] = useState([]);
@@ -20,13 +20,22 @@ function App() {
 
   return (
     <>
-      <Typography component={"h1"} fontSize={"22px"}>
+      <Typography component={"h1"} fontSize={"22px"} mb={"30px"}>
         Commits
       </Typography>
       <Box>
         {commits.length > 0 &&
           commits.map((commit) => {
-            return <Commit key={commit.node_id} data={commit} />;
+            return (
+              <Box key={commit.node_id}>
+                <Commit data={commit} />
+                <Divider
+                  sx={{
+                    background: "#9e9d9d",
+                  }}
+                />
+              </Box>
+            );
           })}
       </Box>
     </>
--- front/src/components/Commit/Commit.jsx
+++ front/src/components/Commit/Commit.jsx
@@ -1,29 +1,36 @@
 import Style from "./Commit.module.css";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
-import { Alert, Avatar, Box, Snackbar, Typography } from "@mui/material";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import {
+  Alert,
+  Avatar,
+  Box,
+  Snackbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 
 import moment from "moment/moment";
 import { useState } from "react";
 
 const Commit = ({ data }) => {
   const [openSnack, setOpenSnack] = useState(false);
+  const [showMore, setShowMore] = useState(false);
   const title = data.commit.message.split("\n").shift();
-  const message = data.commit.message;
+  const messages = data.commit.message.split("\n");
 
   const commitData = {
     title: title,
-    message: message,
+    messages: messages,
     url: data.html_url,
     date: data.commit.committer.date,
-    sha: data.sha, //'7'
+    sha: data.sha,
   };
   const committerData = {
     commiter_name: data.committer.login,
     commiter_avatar: data.committer.avatar_url,
     commiter_redirect: data.committer.html_url,
   };
-
-  console.log(commitData.message, "the rest");
 
   const copyToClipboard = (text) => {
     navigator.clipboard.writeText(text);
@@ -56,11 +63,39 @@ const Commit = ({ data }) => {
         </Alert>
       </Snackbar>
       <Box className={Style.first_col}>
-        <a href={commitData.url} target="_blank" rel="noreferrer">
-          <Typography component={"h2"} fontSize={"20px"}>
-            {commitData.title}
-          </Typography>
-        </a>
+        <Box className={Style.title_container}>
+          <a href={commitData.url} target="_blank" rel="noreferrer">
+            <Typography component={"h2"} fontSize={"18px"} fontWeight={500}>
+              {commitData.title}
+            </Typography>
+          </a>
+          <Tooltip title="Show more..." placement="bottom-start">
+            <button
+              className={Style.icon_buttons}
+              onClick={() => setShowMore(!showMore)}
+            >
+              <MoreHorizIcon
+                sx={{
+                  height: "20px",
+                }}
+              />
+            </button>
+          </Tooltip>
+        </Box>
+        <Box>
+          {showMore &&
+            commitData.messages.map((messages, index) => {
+              return (
+                <Typography
+                  key={index + messages.slice(0, 3)}
+                  component={"p"}
+                  fontSize={"14px"}
+                >
+                  {messages}
+                </Typography>
+              );
+            })}
+        </Box>
 
         <Box className={Style.user_container}>
           <a
@@ -68,7 +103,14 @@ const Commit = ({ data }) => {
             target="_blank"
             rel="noreferrer"
           >
-            <Avatar src={committerData.commiter_avatar} alt="Commiter avatar" />
+            <Avatar
+              src={committerData.commiter_avatar}
+              alt="Commiter avatar"
+              sx={{
+                width: "35px",
+                height: "35px",
+              }}
+            />
           </a>
 
           <a
@@ -76,24 +118,24 @@ const Commit = ({ data }) => {
             target="_blank"
             rel="noreferrer"
           >
-            <Typography component={"h3"} fontSize={"18px"}>
+            <Typography component={"h3"} fontSize={"16px"} fontWeight={500}>
               {committerData.commiter_name}
             </Typography>
           </a>
-          <Typography component={"p"} fontSize={"16px"}>
+          <Typography component={"p"} fontSize={"14px"}>
             Comitted {moment(commitData.date).fromNow()}
           </Typography>
         </Box>
       </Box>
       <Box className={Style.scnd_col}>
         <button
-          className={Style.copy_btn}
+          className={Style.icon_buttons}
           onClick={() => copyToClipboard(commitData.sha)}
         >
           <ContentCopyIcon />
         </button>
         <a href={commitData.url} target="_blank" rel="noreferrer">
-          <Typography component={"p"} fontSize={"16px"}>
+          <Typography component={"p"} fontSize={"14px"} fontWeight={300}>
             {commitData.sha.slice(0, 7)}
           </Typography>
         </a>
