b21951366d7299b74b15788c8d15812917350bfc
Franh01 <fran_lopez9@live.com>
2023-09-12 17:33:32 -0300

    Update front app. Add axios in front. Basic app working.

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [commits, setCommits] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/commits")
+      .then((res) => {
+        setCommits(res.data.commits);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {commits.map((commit) => {
+          console.log(commit, "commit");
+          return (
+            <a
+              key={commit.node_id}
+              href={commit.html_url}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {commit.commit.message}
+            </a>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>hola</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
